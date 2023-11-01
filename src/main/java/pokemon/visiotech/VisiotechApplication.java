5ecfb1a2ae434a328cf27239ccb6f2b36c34c497
Jomaba54 <jomabaz54@gmail.com>
2023-11-01 12:18:12 +0100

    Project initialization

---
+++ src/main/java/pokemon/visiotech/VisiotechApplication.java
@@ -0,0 +1,13 @@
+package pokemon.visiotech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisiotechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisiotechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pokemon/visiotech/VisiotechApplicationTests.java
@@ -0,0 +1,13 @@
+package pokemon.visiotech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisiotechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
